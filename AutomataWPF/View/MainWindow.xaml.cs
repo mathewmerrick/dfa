0b5de7b4b5e463044d2c40b8654230887c31aabd
Mathew Merrick <dev@mathewmerrick.com>
2016-11-09 12:55:56 -0800

    Base WPF Progress

---
+++ AutomataWPF/View/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AutomataApp.ViewModel;
+using AutomataEngine;
+using Microsoft.Win32;
+
+namespace AutomataApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow: Window {
+
+        private MainViewModel VM;
+
+        public MainWindow() {
+            InitializeComponent();
+            VM = new MainViewModel();
+            DataContext = VM;
+        }
+
+
+        private void MenuItemOpen_OnClick(object sender, RoutedEventArgs e) {
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.Filter = "Automata|*.xml";
+
+            try {
+                open.ShowDialog();
+                string filename = open.FileName;
+                VM.load(filename);
+
+                MessageBoxButton button = MessageBoxButton.OK;
+                var result = MessageBox.Show("Loaded Successfully", "Success", button);
+
+            }
+            catch {
+                MessageBoxButton button = MessageBoxButton.OK;
+                var result = MessageBox.Show("Error in loading file", "Error", button);
+            }
+
+
+
+        }
+    }
+}
---
+++ AutomataWPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomataEngine;
+
+namespace AutomataApp.ViewModel {
+    class MainViewModel {
+
+        private AutomataGraph _automata;
+
+        public MainViewModel() {
+            _automata = new AutomataGraph();
+        }
+
+        public void load(string filename) {
+            _automata.Read(filename);
+        }
+
+
+    }
+}
